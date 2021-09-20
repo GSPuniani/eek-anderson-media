71a28f537edc33679dbde6796d21d4a51787e78d
Gobind Puniani <gobindpuniani@gmail.com>
2021-09-20 13:04:01 -0700

    Updated csv cleaner with objects Co-authored-by: tristann3 <tristan15thompson@gmail.com>

--- music_lib_search_tool/music_lib_search_tool/apps/music_collection_search/csv-cleaner.py
+++ music_lib_search_tool/music_lib_search_tool/apps/music_collection_search/csv-cleaner.py
@@ -7,20 +7,27 @@ from temp_models import *
 my_db = set()
 
 
-df = pd.read_csv('master-music.csv')
+df = pd.read_csv('master-music-table.csv')
 
-columns = ["Genre", "Instrument", "Publisher", "Master_Owner", "Mood", "Time_Signature", "Mode", "Keyword", "Production_Style", "Exclusive", "Writer_Split", "Publisher_Split", "Song"]
+columns = ["Genre", "Instrument", "Publisher", "Master_Owner", "Mood", "Time_Signature", "Mode", "Keyword", "Production_Style", "Exclusive", "Writer_Split", "Publisher_Split"]
 columns_lower = list(map(lambda x: x.lower(), columns))
 
+# FOR-LOOP CODE
 # for column in columns_lower:
-#   print(f"""{column}_string = row['{column.title()}'] \n
+#   print(f"""#{column.title()}
+# {column}_string = row['{column.title()}'] \n
+# song_{column}s = []
+
 # if "," in str({column}_string):
 #   {column}s = {column}_string.split(",")
 #   for {column} in {column}s:
-#     {column}s_set.add({column}.strip().title())
+#     {column}_object = {column.title()}({column}.strip().title())
+#     {column}s_set.add({column}_object)
+#     song_{column}s.append({column}_object)
+
 # else:
-#   {column}s_set.add({column}_string.strip.title())
-#   {column}s_set.discard("")""")
+#   {column}s_set.add({column.title()}({column}_string.strip().title()))
+#   {column}s_set.discard("")\n\n""")
 
 
 #reset all sets
@@ -48,182 +55,249 @@ for index, row in df.iterrows():
     # add the object to the song_instruments array
 
 
-#   # Genre
-  genre_string = row['Genre']
+  #Genre
+  genre_string = row['Genre'] 
+
+  song_genres = []
 
   if "," in str(genre_string):
     genres = genre_string.split(",")
     for genre in genres:
-      genres_set.add(genre.strip().title())
+      genre_object = Genre(genre.strip().title())
+      genres_set.add(genre_object)
+      song_genres.append(genre_object)
+
   else:
-    genres_set.add(genre_string.strip().title())
+    genres_set.add(Genre(genre_string.strip().title()))
     genres_set.discard("")
 
 
+  #Instrument
+  instrument_string = row['Instrument'] 
 
-#   #Instrument
-
-# Add object to Table Set and song array
-  instrument_string = row['Instruments']
   song_instruments = []
 
   if "," in str(instrument_string):
     instruments = instrument_string.split(",")
     for instrument in instruments:
-
       instrument_object = Instrument(instrument.strip().title())
       instruments_set.add(instrument_object)
       song_instruments.append(instrument_object)
 
   else:
-    instruments_set.add(instrument_string.strip().title())
+    instruments_set.add(Instrument(instrument_string.strip().title()))
     instruments_set.discard("")
 
 
   #Publisher
-  publisher_string = row['Publisher/Library']
+  publisher_string = row['Publisher'] 
+
+  song_publishers = []
 
   if "," in str(publisher_string):
     publishers = publisher_string.split(",")
     for publisher in publishers:
-      publishers_set.add(publisher.strip().title())
+      publisher_object = Publisher(publisher.strip().title())
+      publishers_set.add(publisher_object)
+      song_publishers.append(publisher_object)
+
   else:
-    publishers_set.add(publisher_string.strip().title())
+    publishers_set.add(Publisher(publisher_string.strip().title()))
     publishers_set.discard("")
 
 
-
   #Master_Owner
-  master_owner_string = row['Master Owner']
+  master_owner_string = row['Master Owner'] 
+
+  song_master_owners = []
 
   if "," in str(master_owner_string):
     master_owners = master_owner_string.split(",")
     for master_owner in master_owners:
-      master_owners_set.add(master_owner.strip().title())
+      master_owner_object = Master_Owner(master_owner.strip().title())
+      master_owners_set.add(master_owner_object)
+      song_master_owners.append(master_owner_object)
+
   else:
-    master_owners_set.add(master_owner_string.strip().title())
+    master_owners_set.add(Master_Owner(master_owner_string.strip().title()))
     master_owners_set.discard("")
 
 
   #Mood
-  mood_string = row['Mood']
+  mood_string = row['Mood'] 
+
+  song_moods = []
 
   if "," in str(mood_string):
     moods = mood_string.split(",")
     for mood in moods:
-      moods_set.add(mood.strip().title())
+      mood_object = Mood(mood.strip().title())
+      moods_set.add(mood_object)
+      song_moods.append(mood_object)
+
   else:
-    moods_set.add(mood_string.strip().title())
+    moods_set.add(Mood(mood_string.strip().title()))
     moods_set.discard("")
 
 
   #Time_Signature
-  time_signature_string = row['Time Signature']
+  time_signature_string = row['Time Signature'] 
+
+  song_time_signatures = []
 
   if "," in str(time_signature_string):
     time_signatures = time_signature_string.split(",")
     for time_signature in time_signatures:
-      time_signatures_set.add(time_signature.strip().title())
+      time_signature_object = Time_Signature(time_signature.strip().title())
+      time_signatures_set.add(time_signature_object)
+      song_time_signatures.append(time_signature_object)
+
   else:
-    time_signatures_set.add(time_signature_string.strip().title())
+    time_signatures_set.add(Time_Signature(time_signature_string.strip().title()))
     time_signatures_set.discard("")
 
 
   #Mode
-  mode_string = row['Mode']
+  mode_string = row['Mode'] 
+
+  song_modes = []
 
   if "," in str(mode_string):
     modes = mode_string.split(",")
     for mode in modes:
-      modes_set.add(mode.strip().title())
+      mode_object = Mode(mode.strip().title())
+      modes_set.add(mode_object)
+      song_modes.append(mode_object)
+
   else:
-    modes_set.add(mode_string.strip().title())
+    modes_set.add(Mode(mode_string.strip().title()))
     modes_set.discard("")
 
 
   #Keyword
-  keyword_string = row['Keywords']
+  keyword_string = row['Keywords'] 
+
+  song_keywords = []
 
   if "," in str(keyword_string):
     keywords = keyword_string.split(",")
     for keyword in keywords:
-      keywords_set.add(keyword.strip().title())
+      keyword_object = Keyword(keyword.strip().title())
+      keywords_set.add(keyword_object)
+      song_keywords.append(keyword_object)
+
   else:
-    keywords_set.add(keyword_string.strip().title())
+    keywords_set.add(Keyword(keyword_string.strip().title()))
     keywords_set.discard("")
 
 
   #Production_Style
-  production_style_string = row['Production Style']
+  production_style_string = row['Production Style'] 
+
+  song_production_styles = []
 
   if "," in str(production_style_string):
     production_styles = production_style_string.split(",")
     for production_style in production_styles:
-      production_styles_set.add(production_style.strip().title())
+      production_style_object = Production_Style(production_style.strip().title())
+      production_styles_set.add(production_style_object)
+      song_production_styles.append(production_style_object)
+
   else:
-    production_styles_set.add(production_style_string.strip().title())
+    production_styles_set.add(Production_Style(production_style_string.strip().title()))
     production_styles_set.discard("")
 
 
   #Exclusive
-  exclusive_string = row['Exclusive / Non Exclusive']
+  exclusive_string = row['Exclusive / Non Exclusive'] 
+
+  song_exclusives = []
 
   if "," in str(exclusive_string):
     exclusives = exclusive_string.split(",")
     for exclusive in exclusives:
-      exclusives_set.add(exclusive.strip().title())
+      exclusive_object = Exclusive(exclusive.strip().title())
+      exclusives_set.add(exclusive_object)
+      song_exclusives.append(exclusive_object)
+
   else:
-    exclusives_set.add(exclusive_string.strip().title())
+    exclusives_set.add(Exclusive(exclusive_string.strip().title()))
     exclusives_set.discard("")
 
 
-  #Writer_Split
-  writer_split_string = row['Writers Split']
+  #Writers_Split
+  writers_split_string = row['Writers Split'] 
 
-  if "," in str(writer_split_string):
-    writer_splits = writer_split_string.split(",")
-    for writer_split in writer_splits:
-      writer_splits_set.add(writer_split.strip().title())
+  song_writers_splits = []
+
+  if "," in str(writers_split_string):
+    writers_splits = writers_split_string.split(",")
+    for writers_split in writers_splits:
+      writers_split_object = Writer_Split(writers_split.strip().title())
+      writer_splits_set.add(writers_split_object)
+      song_writers_splits.append(writers_split_object)
+
   else:
-    writer_splits_set.add(writer_split_string.strip().title())
+    writer_splits_set.add(Writer_Split(writers_split_string.strip().title()))
     writer_splits_set.discard("")
 
 
-  #Publisher_Split
-  publisher_split_string = row['Publishers Split']
+  #Publishers_Split
+  publishers_split_string = row['Publishers Split'] 
 
-  if "," in str(publisher_split_string):
-    publisher_splits = publisher_split_string.split(",")
-    for publisher_split in publisher_splits:
-      publisher_splits_set.add(publisher_split.strip().title())
+  song_publishers_splits = []
+
+  if "," in str(publishers_split_string):
+    publishers_splits = publishers_split_string.split(",")
+    for publishers_split in publishers_splits:
+      publishers_split_object = Publisher_Split(publishers_split.strip().title())
+      publisher_splits_set.add(publishers_split_object)
+      song_publishers_splits.append(publishers_split_object)
+
   else:
-    publisher_splits_set.add(publisher_split_string.strip().title())
+    publisher_splits_set.add(Publisher_Split(publishers_split_string.strip().title()))
     publisher_splits_set.discard("")
 
 
-  #Song
-  song_string = row['Piece Title']
+  # TV Usage
+  has_tv_usage = row["Has TV Usage"]
 
-  if "," in str(song_string):
-    songs = song_string.split(",")
-    for song in songs:
-      songs_set.add(song.strip().title())
+  if has_tv_usage == "checked:":
+    has_tv_usage = 1
   else:
-    songs_set.add(song_string.strip().title())
-    songs_set.discard("")
+    has_tv_usage = 0
+  
 
+  # Stock Sales
+  has_stock_sales = row["Has Stock Music Sales"]
+
+  if has_stock_sales == "checked:":
+    has_stock_sales = 1
+  else:
+    has_stock_sales = 0
+
+
+  # Data Complete
+  is_data_complete = row["Data Complete"]
+
+  if is_data_complete == "checked:":
+    is_data_complete = 1
+  else:
+    is_data_complete = 0
+
+  
   #finally, add song to temp collection
 
-  song = Song(row["Piece Title"], row["Description"], row["Bpm"], row["Duration"], row["Sounds like"], row["Has TV Usage"], row["Has Stock Music Sales"], row["Data Complete"], row["Overall Quality"], row["Key"], genre, instrument, publisher, master_owner, mood, time_signature, mode, keyword, production_style, exclusive, publisher_split, writer_split)
+  song = Song(title=row["Piece Title"], description=row["Description"], bpm=row["Bpm"], duration=row["Duration"], sounds_like=row["Sounds like"], tv_usage=has_tv_usage, stock_sales=has_stock_sales, data_complete=is_data_complete, overall_quality=row["Overall Quality"], music_key=row["Key"], genre=song_genres, instrument=song_instruments, publisher=publisher, owner=master_owner, mood=mood, time_signature=time_signature, mode=mode, keyword=keyword, production_style=production_style, is_exclusive=exclusive, publisher_split=publishers_split, writer_split=writers_split)
 
   my_db.add(song)
 
   print(song)
 
 
-# #helper function to print out each song in the temp set
-# def print_db(new_db):
-#   for song in new_db:
-#     print(song)
+#helper function to print out each song in the temp set
+def print_db(new_db):
+  for song in new_db:
+    print(song)
 
 # print_db(my_db)
