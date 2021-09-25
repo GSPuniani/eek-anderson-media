7d76bc1ff74c135158536f7c98e3a6037205fa5f
Joseph Cottingham <joseph.cottingham@protonmail.com>
2021-09-25 13:58:40 -0400

    migrations

--- music_lib_search_tool/music_lib_search_tool/apps/music_collection_search/views.py
+++ music_lib_search_tool/music_lib_search_tool/apps/music_collection_search/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django.db import connection
 
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+
+def run_db_query(query, args):
+    with connection.cursor() as cursor:
+        cursor.execute(query, args)
+        result = dictfetchall(cursor)
+    return result
 
 
 class Search_View(View):
+
+    def get(self, request):
+        context = {}
+        return render(request, 'music_collection_search/Search_View.html', context)
 
     def get(self, request):
         context = {}
